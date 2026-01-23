60f4816ed9bf86267912df6f1e3b592d01989d3c
Alva Romeo 999 <133835563+Alvaromeo999@users.noreply.github.com>
2026-01-24 03:48:48 +0700

    Update script.js
    Signed-off-by: Alva Romeo 999 <133835563+Alvaromeo999@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 // 1. KONFIGURASI SUPABASE (VERSI BROWSER)
 // ===============================
 // Masukkan URL dan Key Anon kamu langsung di sini (String)
-const supabaseUrl = 'https://tosjjicxibibuxpskpjz.supabase.co'
-NEXT_PUBLIC_SUPABASE_URL=https://tosjjicxibibuxpskpjz.supabase.co
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY=sb_publishable_gTmur1J62LkEE4nG8EE_pg_2YFwV7nf'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvc2pqaWN4aWJpYnV4cHNrcGp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxODAxMTAsImV4cCI6MjA4NDc1NjExMH0.-wAYdccN8Ji6tVWhXYQrhJunDeyA7cpzskkmpY3MLT0...' // <-- GANTI DENGAN KEY PANJANG DARI DASHBOARD
+
+const supabase Url = 'https://tosjjicxibibuxpskpjz.supabase.co'
+const Publishable Key ='sb_publishable_gTmur1J62LkEE4nG8EE_pg_2YFwV7nf'
+const supabase Anon Key (Legacy) = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvc2pqaWN4aWJpYnV4cHNrcGp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxODAxMTAsImV4cCI6MjA4NDc1NjExMH0.-wAYdccN8Ji6tVWhXYQrhJunDeyA7cpzskkmpY3MLT0...' // <-- GANTI DENGAN KEY PANJANG DARI DASHBOARD
 
 // Pastikan library supabase sudah dimuat di HTML (cek langkah 2 di bawah)
 const sb = supabase.createClient(supabaseUrl, supabaseKey)
