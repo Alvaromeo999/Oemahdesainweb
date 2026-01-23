01e491fd1a7899dac767a63c328ee84eed217b4d
Alva Romeo 999 <133835563+Alvaromeo999@users.noreply.github.com>
2026-01-24 03:11:01 +0700

    Update script.js
    Signed-off-by: Alva Romeo 999 <133835563+Alvaromeo999@users.noreply.github.com>

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // ===============================
 // Masukkan URL dan Key Anon kamu langsung di sini (String)
 const supabaseUrl = 'https://tosjjicxibibuxpskpjz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...' // <-- GANTI DENGAN KEY PANJANG DARI DASHBOARD
+const supabaseKey = 'sb_publishable_gTmur1J62LkEE4nG8EE_pg_2YFwV7nf...' // <-- GANTI DENGAN KEY PANJANG DARI DASHBOARD
 
 // Pastikan library supabase sudah dimuat di HTML (cek langkah 2 di bawah)
 const sb = supabase.createClient(supabaseUrl, supabaseKey)
