a48a5ef8e0b00e106cef54730446fd6b91c31a68
Alva Romeo 999 <133835563+Alvaromeo999@users.noreply.github.com>
2026-01-23 22:34:34 +0700

    Create script.js
    Signed-off-by: Alva Romeo 999 <133835563+Alvaromeo999@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,33 @@
+let selectedRating = 0;
+
+// Rating
+document.querySelectorAll('.stars span').forEach(star => {
+  star.addEventListener('click', function () {
+    selectedRating = this.dataset.value;
+    document.querySelectorAll('.stars span').forEach(s => s.classList.remove('active'));
+    for (let i = 0; i < selectedRating; i++) {
+      document.querySelectorAll('.stars span')[i].classList.add('active');
+    }
+  });
+});
+
+// Review
+function submitReview() {
+  const text = document.getElementById('reviewText').value;
+  if (!text || selectedRating == 0) {
+    alert('Isi rating & review dulu ya 🙂');
+    return;
+  }
+
+  document.getElementById('reviewList').innerHTML += `
+    <p>⭐ ${selectedRating}/5<br>${text}</p><hr>
+  `;
+  document.getElementById('reviewText').value = '';
+}
+
+// Visitor counter
+let count = localStorage.getItem('visitorCount') || 0;
+count++;
+localStorage.setItem('visitorCount', count);
+document.getElementById('visitorCount').innerText = count;
+                        
